170b6d781c311d6db635df44bec1479f354b1a2f
muendo <horracemuendo34.com>
2024-02-10 23:54:10 +0300

    working on customer list

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          hello does react run on live server lemme try 
+          shii it takes forever to reload fucking react how tf ami supposed to code in this
+          idk how but the perfomance keeps on improving periodically
+          okay this live reload feature is  awesome time to start coding 
         </p>
         <a
           className="App-link"
