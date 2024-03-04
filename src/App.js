e59f847ed6e30fe2763171825d9ee28acfdc5e7f
muendo <horracemuendo34.com>
2024-03-04 16:29:23 +0300

    working on customer detals

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
+import CustomerList from './Components/CustomerList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-          hello does react run on live server lemme try 
-          shii it takes forever to reload fucking react how tf ami supposed to code in this
-          idk how but the perfomance keeps on improving periodically
-          okay this live reload feature is  awesome time to start coding 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomerList />
+   
+      
     </div>
   );
 }
