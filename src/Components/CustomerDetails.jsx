c7abc6eba58ddeec7bd97d44bc4a55cb5165c34a
muendo <horracemuendo34.com>
2024-03-04 16:38:23 +0300

    working on the customer conditoinals

---
+++ src/Components/CustomerDetails.jsx
@@ -0,0 +1,18 @@
+import { React } from "react";
+
+function CustomerDetails({ customer }) {
+    return (
+        <div className="customer-details">
+            <h2>Customer Details</h2>
+            {
+                customer ? (
+                    <div>
+                        
+                    </div>
+                )
+
+            }
+        </div>
+    );
+    
+}
