626555d26a915d6168934e8d2263c124ab08e392
muendo <horracemuendo34.com>
2024-06-30 20:38:09 +0300

    making some changes here and there

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
+// import 'remixicons/fonts/remixicon.css'
+// Enabling bootstrap js and css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 import './App.css';
+import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-   <Dashboard />
+      <Header />
+      <Dashboard />
       
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/header.css'
+
+function Header() {
+  return (
+      <Header id = 'header' className='header fixed-top d-flex align-items-center'>
+          Mteja CRM
+      </Header>
+  )
+}
+
+export default Header;
