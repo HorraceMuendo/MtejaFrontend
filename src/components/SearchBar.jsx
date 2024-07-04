88b51fa84af01a7d392962698396b3e3a4687fc6
muendo <horracemuendo34.com>
2024-07-04 23:11:31 +0300

    solved the typo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import '../styles/searchbar.css'
+import '../styles/searchBar.css'
 
 function SearchBar() {
-  return (
-      <div className='search-bar'>
-          <form
-              className='search-from d-flex align-items-center'
-              method='POST'
-              action="#">
-              <input type="text" name='query' placeholder='Search' title='Enter key words' />
-              <button type='submit' title='Search'>
-                  <i className='bi bi-search'></i>
-              </button>
+    return (
+        <div className='search-bar'>
+            <form
+                className='search-form d-flex align-items-center'
+                method='POST'
+                action="#">
+                <input type="text" name='query' placeholder='Search' title='Enter key words' />
+                <button type='submit' title='Search'>
+                    <i className='bi bi-search'></i>
+                </button>
 
 
-              </form>
+            </form>
           
-    </div>
-  )
+        </div>
+    );
 }
 
 export default SearchBar
