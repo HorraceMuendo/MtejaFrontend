acd6cedf5a3640adf18957dcda711a9ff2ccaf05
muendo <horracemuendo34.com>
2024-07-25 00:21:33 +0300

    changing the codebase

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import 'bootstrap-icons/font/bootstrap-icons.css'
-// import 'remixicons/fonts/remixicon.css'
-// Enabling bootstrap js and css
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
+import './App.css'
+import Header from './Components/Header'
+import Sidebar from './Components/Sidebar'
+import Home from './Components/Home'
 
-
-
-
-import './App.css';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DashboardPages from './pages/dashboard/DashboardPages';
-import PageNotFound from './pages/error/PageNotFound';
-import Layout from './layout/Layout';
 function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route element={<Layout/>}>
-            <Route path='/' element={<DashboardPages />} />
-            <Route path='*' element={<PageNotFound/>} />
-          </Route>
-        </Routes>
-      </Router>
-  
-    </>
-  );
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 reportWebVitals();
