bf2e376e7b45a6974e0cf9ac6e4dd134646030a6
Horrace Muendo <horracemuendo34@gmail.com>
2024-09-22 05:49:13 +0300

    changing 80% of the code base

--- src/App.js
+++ src/App.js
@@ -1,84 +1,6 @@
-// import { useState } from 'react'
-// import './App.css'
-// import Header from './Components/Header'
-// import Sidebar from './Components/Sidebar'
-// import Home from './Components/Home'
-// import User from './pages/UserProfile'
-// import Signup from './pages/SignUp'
-// import Login from './pages/Login'
-
-// function App() {
-//   const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
-
-//   const OpenSidebar = () => {
-//     setOpenSidebarToggle(!openSidebarToggle)
-//   }
-
-//   return (
-//     <div className='grid-container'>
-//       <Header OpenSidebar={OpenSidebar}/>
-//       <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-//       <Home />
-//       <User />
-//       <Signup />
-//       <Login/>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import './App.css';
-// import Header from './Components/Header';
-// import Sidebar from './Components/Sidebar';
-// import Home from './Components/Home';
-// import User from './pages/UserProfile';
-// import Signup from './pages/SignUp';
-// import Login from './pages/Login';
-// import LandingPage from './pages/LandingPage';
-
-// function App() {
-//   const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
-
-//   const OpenSidebar = () => {
-//     setOpenSidebarToggle(!openSidebarToggle);
-//   }
-
-//   return (
-//     <Router>
-//       <div className='grid-container'>
-//         <Header OpenSidebar={OpenSidebar} />
-//         <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar} />
-//         <div className='main-content'>
-//           <Routes>
-//             <Route path="/signup" element={<Signup />} />
-//             <Route path="/login" element={<Login />} />
-//             <Route path="/home" element={<Home />} />
-//             <Route path="/user" element={<User />} />
-//             <Route path="/" element={<LandingPage />} />
-//           </Routes>
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
-import Sidebar from './Components/Sidebar';
-import Home from './Components/Home';
 import User from './pages/UserProfile';
 import Signup from './pages/SignUp';
 import Login from './pages/Login';
@@ -86,46 +8,25 @@ import LandingPage from './pages/LandingPage';  // Import LandingPage
 import Reports from './pages/Reports';
 import ProductList from './pages/Products/ProductList';
 import AddProduct from './pages/Products/AddProduct';
-import Categories from './pages/Products/Categories';
-import CommunicationLogs from './Components/Clients/CommunicationLogs';
 
 function App() {
-  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
-
-  const OpenSidebar = () => {
-    setOpenSidebarToggle(!openSidebarToggle);
-  }
-
+  
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />  {/* Landing Page Route */}
+        <Route path="/" element={<LandingPage />} /> 
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route
           path="*"
           element={
             <div className='grid-container'>
-              <Header OpenSidebar={OpenSidebar} />
-              <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar} />
+              <Header/>
               <Routes>
-                <Route path="/home" element={<Home />} />
                 <Route path="/products/list" element={<ProductList/>} />
                 <Route path="/products/add" element={<AddProduct/>} />
-                <Route path="/products/categories" element={<Categories/>} />
                 <Route path="/user" element={<User />} />
-                <Route path="/reports" element={<Reports/>} />
-                {/* Add other routes as needed */}
-                <Route path="/communication" element={<CommunicationLogs />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-
-                
-
-
-
-
+                <Route path="/reports" element={<Reports/>} />              
               </Routes>
             </div>
           }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,35 @@
+import React from 'react';
+import { Navbar, Nav,NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../styles/Header.css';
+import logo from './Images/logo.jpg';
 
-import React from 'react'
-import "../styles/Header.css"
-import 
- {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsJustify}
- from 'react-icons/bs'
-
-function Header({OpenSidebar}) {
+function Header() {
   return (
-    <header className='header'>
-        <div className='menu-icon'>
-            <BsJustify className='icon' onClick={OpenSidebar}/>
-        </div>
-        <div className='header-left'>
-              Naivas CRM
-        </div>
-        <div className='header-right'>
-           
-            <BsPersonCircle className='icon'/>
-        </div>
-    </header>
-  )
+    <Navbar bg="light" expand="lg" className="header fixed-top">
+      <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+        <img src={logo} alt="Logo" className="logo-image d-inline-block align-top" />
+        <span className="d-none d-lg-block ml-2">NaivasCRM</span>
+      </Navbar.Brand>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+          <NavDropdown title="Products" id="products-dropdown">
+            <NavDropdown.Item as={Link} to="/products/list">List</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/products/add">Add</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link as={Link} to="/enquiries">Enquiries</Nav.Link>
+          <NavDropdown title="Customer" id="customer-dropdown">
+            <NavDropdown.Item as={Link} to="/compliments">Compliments</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/complaints">Complaints</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link as={Link} to="/enquiries">Reports</Nav.Link>
+          <Nav.Link as={Link} to="/contactus">Account</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
